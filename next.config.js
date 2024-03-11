a767ab5163020596bb3a0fcddbdc941cca1108ab
Lord Frantex <lordfranktex@gmail.com>
2024-03-11 14:10:45 +0100

    fixing env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const path = require('path')
 
 const nextConfig = {
     reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_BASE_URL: process.env.BASE_URL,
+    }
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
